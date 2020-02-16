a49ea572b16061d947d937417e42282a7715f696
Eric <eric.canas@e-campus.uab.cat>
2020-02-16 01:39:39 +0100

    Fit step completed

--- UnsupervisedAndReinforcementLearning/LargeDatasetKmeans.py
+++ UnsupervisedAndReinforcementLearning/LargeDatasetKmeans.py
@@ -1,5 +1,5 @@
 import numpy as np
-import math
+
 class RecursivePartitionKmeans:
     """Recursive Partition based K-Means clustering (RPKM).
     Read more in :ref:  Basu, S.; Banerjee, A. & Mooney, R. J.
@@ -8,83 +8,31 @@ class RecursivePartitionKmeans:
                         2002, 27-34
     Parameters
     ----------
-    n_clusters : int, default=8
+    n_clusters : int, default=4
         The number of clusters to form as well as the number of
         centroids to generate.
     max_iter : int, default=300
         Maximum number of iterations of the Recursive Partition based K-means
-        (RPKM) algorithm for a single run.
+        (RPKM) algorithm for a each Weighted Lloyd's Algorithm step.
 
-    init : {'k-means++', 'random'} or ndarray of shape \
-            (n_clusters, n_features), default='k-means++'
-        Method for initialization, defaults to 'k-means++':
-        'k-means++' : selects initial cluster centers for k-mean
-        clustering in a smart way to speed up convergence. See section
-        Notes in k_init for more details.
-        'random': choose k observations (rows) at random from data for
-        the initial centroids.
-        If an ndarray is passed, it should be of shape (n_clusters, n_features)
-        and gives the initial centers.
-    n_init : int, default=10
-        Number of time the k-means algorithm will be run with different
-        centroid seeds. The final results will be the best output of
-        n_init consecutive runs in terms of inertia.
-    tol : float, default=1e-4
-        Relative tolerance with regards to inertia to declare convergence.
-    precompute_distances : 'auto' or bool, default='auto'
-        Precompute distances (faster but takes more memory).
-        'auto' : do not precompute distances if n_samples * n_clusters > 12
-        million. This corresponds to about 100MB overhead per job using
-        double precision.
-        True : always precompute distances.
-        False : never precompute distances.
-    verbose : int, default=0
-        Verbosity mode.
-    random_state : int, RandomState instance, default=None
-        Determines random number generation for centroid initialization. Use
-        an int to make the randomness deterministic.
-        See :term:`Glossary <random_state>`.
-    copy_x : bool, default=True
-        When pre-computing distances it is more numerically accurate to center
-        the data first.  If copy_x is True (default), then the original data is
-        not modified, ensuring X is C-contiguous.  If False, the original data
-        is modified, and put back before the function returns, but small
-        numerical differences may be introduced by subtracting and then adding
-        the data mean, in this case it will also not ensure that data is
-        C-contiguous which may cause a significant slowdown.
-    n_jobs : int, default=None
-        The number of jobs to use for the computation. This works by computing
-        each of the n_init runs in parallel.
-        ``None`` means 1 unless in a :obj:`joblib.parallel_backend` context.
-        ``-1`` means using all processors. See :term:`Glossary <n_jobs>`
-        for more details.
-    algorithm : {"auto", "full", "elkan"}, default="auto"
-        K-means algorithm to use. The classical EM-style algorithm is "full".
-        The "elkan" variation is more efficient by using the triangle
-        inequality, but currently doesn't support sparse data. "auto" chooses
-        "elkan" for dense data and "full" for sparse data.
+    error_th : float, default=1e-4
+        Min distance between C_{i-1} and C_i to consider convergence.
+    verbose : int, default=False
+        Plot graphics at each step (It will provoke stops at each step)
+
     Attributes
     ----------
     cluster_centers_ : ndarray of shape (n_clusters, n_features)
-        Coordinates of cluster centers. If the algorithm stops before fully
-        converging (see ``tol`` and ``max_iter``), these will not be
-        consistent with ``labels_``.
+        Coordinates of cluster centers.
     labels_ : ndarray of shape (n_samples,)
         Labels of each point
-    inertia_ : float
-        Sum of squared distances of samples to their closest cluster center.
     n_iter_ : int
         Number of iterations run.
-    See Also
-    --------
-    MiniBatchKMeans
-        Alternative online implementation that does incremental updates
-        of the centers positions using mini-batches.
-        For large scale learning (say n_samples > 10k) MiniBatchKMeans is
-        probably much faster than the default batch implementation.
     Notes
     -----
-    The k-means problem is solved using either Lloyd's or Elkan's algorithm.
+    The RPKM is solved running weighted Lloyd's Algorithm.
+    The initialization proposed by his original paper is always Forgy's (random),
+    it is not dangerous given that the partitions are distributed in the space.
     The RPKM algorithm updates k-means in order to solve the problem when
     datasets are extremely large.
     It uses Forgy's inizialization for setting the first K random representatives.
@@ -97,29 +45,16 @@ class RecursivePartitionKmeans:
     generated at this iteration, K to the n_clusters parameter, and d to the dimensions
     of the dataset.
     The partitions of the Dataset (D to P_i) are done using quadtrees.
-    Examples
-    --------
-
     """
 
-    def __init__(self, n_clusters=8, init='k-means++', n_init=10,
-                 max_iter=300, tol=1e-4, precompute_distances='auto',
-                 verbose=0, random_state=None, copy_x=True,
-                 n_jobs=None, algorithm='auto'):
-
+    def __init__(self, n_clusters=4, max_iter=300, error_th = 1e-5,
+                 verbose=False):
         self.n_clusters = n_clusters
-        self.init = init
         self.max_iter = max_iter
-        self.tol = tol
-        self.precompute_distances = precompute_distances
-        self.n_init = n_init
+        self.error_th = error_th
         self.verbose = verbose
-        self.random_state = random_state
-        self.copy_x = copy_x
-        self.n_jobs = n_jobs
-        self.algorithm = algorithm
 
-    def fit(self, X, y=None, sample_weight=None):
+    def fit(self, X, y=None):
         """Compute Recursive Partition based k-means (RPKM) clustering.
         Parameters
         ----------
@@ -129,9 +64,6 @@ class RecursivePartitionKmeans:
             copy if the given data is not C-contiguous.
         y : Ignored
             Not used, present here for API (scikit-learn) consistency by convention.
-        sample_weight : array-like, shape (n_samples,), optional
-            The weights for each observation in X. If None, all observations
-            are assigned equal weight (default: None).
         Returns
         -------
         self
@@ -139,7 +71,7 @@ class RecursivePartitionKmeans:
         """
         #--------------------------------------------------------------------------------------------
         #Step 1: Construct an initial partition of X, P, and define an initial set of K centroids, C.
-            #Step 1.1: Spatial Partition: Based on grid based RPKM (see section 2.4 (original paper))
+        #Step 1.1: Spatial Partition: Based on grid based RPKM (see section 2.4 (original paper))
 
         #Delimite the initial bounding box
         self.bounding_box = np.array([[np.min(X[:,d]), np.max(X[:,d])]
@@ -149,26 +81,40 @@ class RecursivePartitionKmeans:
         X[X[..., [0, 1]] == self.bounding_box[:, 1]] -= epsilon
         #Constraint: Partitions (with representatives) > K
         weights = []
+        iter = 1
         while(len(weights)< self.n_clusters):
             # Get the partition correspondences (P) the representative of each centroid, and the weights (|S|)
-            P, representatives, weights = self.partitionate_dataset(X, iter=1)
-            #Step 1.2: Construct the initial set of K centroids
+            P, representatives, weights = self.partitionate_dataset(X, iter=iter)
+            iter += 1
+
+        #Step 1.2: Construct the initial set of K centroids
         C = self.forgys_cluster_initialization(representatives,self.n_clusters)
-        C = self.weighted_lloyd(P=representatives,C=C,weights=weights)
+
         # --------------------------------------------------------------------------------------------
-        #Step 2: Weighted Lloyd
+        # Step 2: Initial Weighted Lloyd
+        C, G = self.weighted_lloyd(P=representatives,C=C,weights=weights,thr=self.error_th)
+        distance = self.error_th + 1
+        while iter < self.max_iter and distance > self.error_th:
+            # ----------------------------------------------------------------------------------------
+            # Step 3: Construct a dataset partition P' thinner than P
+            P, representatives, weights = self.partitionate_dataset(X, iter=iter)
 
+            # ----------------------------------------------------------------------------------------
+            # Step 4: Weighted Lloyd
+            C_new, G = self.weighted_lloyd(P=representatives, C=C, weights=weights, thr=self.error_th)
+            distance  = np.sum(np.abs(C_new-C))
+            iter += 1
+            C = C_new
 
-        """
-        self.cluster_centers_ = best_centers
-        self.labels_ = best_labels
-        self.inertia_ = best_inertia
-        self.n_iter_ = best_n_iter
-        """
+        self.cluster_centers_ = C
+        self.labels_ = self.WL_assignment_step(P, C=C)
+        self.n_iter_ = iter-1
+
         return self
 
     def partitionate_dataset(self, X, iter):
-        """Compute the dataset partition for dataset X at iteration iter.
+        """
+        Compute the dataset partition for dataset X at iteration iter.
         At first iteration, partition is defined by the grid obtained after dividing each side
         of the smallest bounding box of X by half. In following iterations it will be 2**i partitions.
 
@@ -205,7 +151,7 @@ class RecursivePartitionKmeans:
                           dtype=X.dtype)
         return d_labels, representatives, weights
 
-    def weighted_lloyd(self, P, C, weights):
+    def weighted_lloyd(self, P, C, weights, thr = 0.):
         """
         Compute the Weighted Lloyd algorithm for the Partition P, using C as initial clusters.
         For the PRKM case, weights of P are defined as his cardinality (|S|)
@@ -220,11 +166,88 @@ class RecursivePartitionKmeans:
             Clusters of the last iteration
         weights : array-like or matrix, shape=(len(P))
                   |S|
+        thr : distance thr over the evolution of cluster for considered as converged.
+              If set to negative value, no stop condition other than max_iterations.
         Returns
         -------
-        Clusters (C) and Associations (G) which minimize the error
+        Clusters (C) which minimize the error Associations (G)
         """
-        
+        #Step 0 : Initial Assignment
+        G = self.WL_assignment_step(P, C=C)
+
+        distance, r = thr+1, 0
+        while r<self.max_iter and distance>thr:
+            #Step 1: Update Step (C_r <- G_{r-1})
+            C_new = self.WL_update_step(P=P, G=G, weights=weights, C=C)
+
+            #Step 2: Assignment Step (G_r <- C_r)
+            G = self.WL_assignment_step(P, C=C_new)
+
+            #Update variables and constraints
+            distance  = np.sum(np.abs(C_new-C))
+            r += 1
+            C = C_new
+
+        return C, G
+
+    def WL_update_step(self,P, G, weights, C):
+        """
+        Compute the assignment step of Weighted Lloyd algorithm for the Partition P, using C as initial clusters.
+        Parameters
+        ----------
+        P : array-like or sparse matrix, shape=(n_partitions, n_features)
+            Array of representatives of each P_i at the current iteration
+        G : array-like, shape=(n_partitions)
+            Array with the closest cluster id for each point
+        weights : array-like or matrix, shape=(len(P))
+                  |S|
+        C : array-like or matrix, shape=(K, n_features)
+            Clusters of the last iteration
+        Returns
+        -------
+        Clusters (C) updated
+        """
+        C_new = np.array(C, copy=True)
+        for moved_cluster_id in np.unique(G):
+            mask = G==moved_cluster_id
+            c_points, c_weights = P[mask], weights[mask]
+            C_new[moved_cluster_id] = np.sum((c_points.T*c_weights),axis=1)/np.sum(c_weights)
+        return C_new
+
+    def WL_assignment_step(self, P, C):
+        """
+        Compute the assignment step of Weighted Lloyd algorithm for the Partition P, using C as initial clusters.
+        Parameters
+        ----------
+        P : array-like or sparse matrix, shape=(n_partitions, n_features)
+            Array of representatives of each P_i at the current iteration
+        weights : array-like or matrix, shape=(len(P))
+                  |S|
+        C : array-like or matrix, shape=(K, n_features)
+            Clusters of the last iteration
+        Returns
+        -------
+        New Associations (G)
+        """
+
+        distances = np.array([self.euclidean_distance(points=P, centroid=centroid) for centroid in C])
+        G = np.argmin(distances, axis=0)
+        return G
+
+    def euclidean_distance(self, points, centroid):
+        """
+        Compute the euclidean distance between a set of points and a centroid.
+        Parameters
+        ----------
+        points : array-like or sparse matrix, shape=(n_points, n_features)
+                Array of points
+        centroid : array-like or matrix, shape=(n_features)
+                   Given cluster
+        Returns
+        -------
+        Array of distances
+        """
+        return np.sqrt(np.sum(np.square((points - centroid)), axis=1))
 
     def forgys_cluster_initialization(self,X, K):
         """Get the Forgy's inicialization of clusters. Which is basically to select
@@ -241,7 +264,8 @@ class RecursivePartitionKmeans:
         Array of K x dimensions(X) features which represents the selected clusters.
         """
         return X[np.random.choice(len(X), size=K)]
-    def fit_predict(self, X, y=None, sample_weight=None):
+
+    def fit_predict(self, X, y=None):
         """Compute cluster centers and predict cluster index for each sample.
         Convenience method; equivalent to calling fit(X) followed by
         predict(X).
@@ -251,55 +275,14 @@ class RecursivePartitionKmeans:
             New data to transform.
         y : Ignored
             Not used, present here for API (scikit-learn) consistency by convention.
-        sample_weight : array-like, shape (n_samples,), optional
-            The weights for each observation in X. If None, all observations
-            are assigned equal weight (default: None).
         Returns
         -------
         labels : array, shape [n_samples,]
             Index of the cluster each sample belongs to.
         """
-        return self.fit(X, sample_weight=sample_weight).labels_
+        return self.fit(X).labels
 
-    def fit_transform(self, X, y=None, sample_weight=None):
-        """Compute clustering and transform X to cluster-distance space.
-        Equivalent to fit(X).transform(X), but more efficiently implemented.
-        Parameters
-        ----------
-        X : {array-like, sparse matrix} of shape (n_samples, n_features)
-            New data to transform.
-        y : Ignored
-            Not used, present here for API consistency by convention.
-        sample_weight : array-like, shape (n_samples,), optional
-            The weights for each observation in X. If None, all observations
-            are assigned equal weight (default: None).
-        Returns
-        -------
-        X_new : array, shape [n_samples, k]
-            X transformed in the new space.
-        """
-        return self.fit(X, sample_weight=sample_weight)._transform(X)
-
-    def transform(self, X):
-        """Transform X to a cluster-distance space.
-        In the new space, each dimension is the distance to the cluster
-        centers.  Note that even if X is sparse, the array returned by
-        `transform` will typically be dense.
-        Parameters
-        ----------
-        X : {array-like, sparse matrix} of shape (n_samples, n_features)
-            New data to transform.
-        Returns
-        -------
-        X_new : array, shape [n_samples, k]
-            X transformed in the new space.
-        """
-        return self.euclidean_distances(X, self.cluster_centers_)
-
-    def euclidean_distances(X, cluster_centers):
-        pass
-
-    def predict(self, X, sample_weight=None):
+    def predict(self, X):
         """Predict the closest cluster each sample in X belongs to.
         In the vector quantization literature, `cluster_centers_` is called
         the code book and each value returned by `predict` is the index of
@@ -308,14 +291,12 @@ class RecursivePartitionKmeans:
         ----------
         X : {array-like, sparse matrix} of shape (n_samples, n_features)
             New data to predict.
-        sample_weight : array-like, shape (n_samples,), optional
-            The weights for each observation in X. If None, all observations
-            are assigned equal weight (default: None).
         Returns
         -------
         labels : array, shape [n_samples,]
             Index of the cluster each sample belongs to.
         """
+
         """
         check_is_fitted(self)
 
@@ -323,29 +304,4 @@ class RecursivePartitionKmeans:
         x_squared_norms = row_norms(X, squared=True)
         return _labels_inertia(X, sample_weight, x_squared_norms,
                                self.cluster_centers_)[0]
-        """
-
-    def score(self, X, y=None, sample_weight=None):
-        """Opposite of the value of X on the K-means objective.
-        Parameters
-        ----------
-        X : {array-like, sparse matrix} of shape (n_samples, n_features)
-            New data.
-        y : Ignored
-            Not used, present here for API consistency by convention.
-        sample_weight : array-like, shape (n_samples,), optional
-            The weights for each observation in X. If None, all observations
-            are assigned equal weight (default: None).
-        Returns
-        -------
-        score : float
-            Opposite of the value of X on the K-means objective.
-        """
-        """
-        check_is_fitted(self)
-
-        X = self._check_test_data(X)
-        x_squared_norms = row_norms(X, squared=True)
-        return -_labels_inertia(X, sample_weight, x_squared_norms,
-                                self.cluster_centers_)[1]
         """
